95d0887ce5b2026c756cd6280f67c0134be82adb
spmcadam <spmcadam57@gmail.com>
2025-03-29 11:52:05 -0700

    Add story,photos, update style, index,rocket

--- slider.js
+++ slider.js
@@ -1,23 +1,52 @@
 // slider.js
 
-// Grab all slider containers
+// Select all slider containers
 const sliderContainers = document.querySelectorAll(".slider-container");
 
-// We’ll keep an array of slider states
-const sliders = [];
-
-// For each container, gather slides, track the current index, set up an interval
-sliderContainers.forEach(container => {
+sliderContainers.forEach((container) => {
+  // All the .slide elements in this container
   const slides = container.querySelectorAll(".slide");
+  if (!slides.length) return; // If no slides, skip
+
+  // Optional: find arrows in the same container if they exist
+  const leftArrow = container.parentNode.querySelector(".arrow.left");
+  const rightArrow = container.parentNode.querySelector(".arrow.right");
+
   let currentSlide = 0;
 
   // Show the first slide
   slides[currentSlide].classList.add("active");
 
-  // Every 3 seconds, fade to the next
-  setInterval(() => {
+  // Automatically cycle every 7 seconds
+  let intervalId = setInterval(nextSlide, 7000);
+
+  function nextSlide() {
     slides[currentSlide].classList.remove("active");
     currentSlide = (currentSlide + 1) % slides.length;
     slides[currentSlide].classList.add("active");
-  }, 7000); // 7 seconds
+  }
+
+  function prevSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }
+
+  // If the container has arrows, hook them up
+  if (leftArrow && rightArrow) {
+    leftArrow.addEventListener("click", () => {
+      prevSlide();
+      resetInterval();
+    });
+    rightArrow.addEventListener("click", () => {
+      nextSlide();
+      resetInterval();
+    });
+  }
+
+  // A helper to stop and restart the timer, so user clicks don’t immediately get overwritten
+  function resetInterval() {
+    clearInterval(intervalId);
+    intervalId = setInterval(nextSlide, 7000);
+  }
 });
